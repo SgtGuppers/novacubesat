82190ce2072201c8b72776ab1ec666c680b5ddae
SgtGuppers <76712415+SgtGuppers@users.noreply.github.com>
2024-02-08 21:12:00 -0500

    Update FlatSat_student.py

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -78,7 +78,7 @@ def take_photo():
 
             camera_config = picam2.create_preview_configuration()
             picam2.configure(camera_config)
-            picam2.start_preview(Preview.DRM)
+            picam2.start_preview(Preview.NULL)
             picam2.start()
             time.sleep(2)
             picam2.capture_file("test.jpg")
